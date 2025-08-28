096a9d9d00c4e5ba1421a2e94db740c3d0a52fb7
niranjan <niranjan@7unique.in>
2025-08-28 14:38:12 +0530

    UI

--- components/BankingPartner.js
+++ components/BankingPartner.js
@@ -14,16 +14,18 @@ const partnerLogos = [
 
 export default function BankingPartner() {
   return (
-    <div className="w-full py-12 bg-white"   data-aos="fade-right"
->
+    <div className="w-full py-12 bg-white" data-aos="fade-right">
       <div className="container mx-auto px-4">
         <div className="flex flex-col lg:flex-row items-start justify-between gap-10">
           <div className="lg:max-w-md">
-            <p className="text-5xl font-semibold">
-              <span className="theme-text "> Our Banking &</span> <br></br> Partner Network
+            <p className="text-5xl font-bold">
+              <span className="theme-text "> Our Banking &</span> <br></br>{" "}
+              Partner Network
             </p>
-            <p className="text-gray-600 mt-4 text-base leading-relaxed">
-              At vero eos et accusamus et iusto odio dignissimos ducimus odio dignissimos ducimus At vero eos et accusamus et iusto odio dignissimos ducimus odio dignissimos ducimus
+            <p className="text-gray-600 mt-4 text-base leading-relaxed  font-medium">
+              At vero eos et accusamus et iusto odio dignissimos ducimus odio
+              dignissimos ducimus At vero eos et accusamus et iusto odio
+              dignissimos ducimus odio dignissimos ducimus
             </p>
           </div>
 
@@ -31,8 +33,9 @@ export default function BankingPartner() {
             {partnerLogos.map((logo, i) => (
               <div
                 key={i}
-                className={`group relative w-32 h-24 shadow-[0_4px_30px_rgba(228,237,243,1)] rounded-lg bg-white transition-all duration-500 hover:bg-[#2E709B] flex items-center justify-center float-animate ${i % 2 === 0 ? "mt-0" : "mt-10"
-                  }`}
+                className={`group relative w-32 h-24 shadow-[0_4px_30px_rgba(228,237,243,1)] rounded-lg bg-white transition-all duration-500 hover:bg-[#2E709B] flex items-center justify-center float-animate ${
+                  i % 2 === 0 ? "mt-0" : "mt-10"
+                }`}
                 style={{
                   animationDelay: `${i * 0.3}s`,
                   animationDuration: `${6 + (i % 4)}s`,
